e8630c363a32fd922cee8bfc99e21399dc0ed45e
David Gras <davidgrcr@gmail.com>
2022-06-22 22:56:44 +0200

    Adding test for ui components with functionality

--- src/components/ui/Button/Button.test.tsx
+++ src/components/ui/Button/Button.test.tsx
@@ -1,19 +1,20 @@
-import { describe, expect, test, vi } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { fireEvent, render, screen } from "@testing-library/react";
 import Button from "./Button";
 
 describe("Button test", () => {
-  test("should show title all the time", () => {
-    const handleClick = vi.fn();
-
+  const handleClick = vi.fn();
+  beforeEach(() => {
     render(
       <Button onClick={handleClick} type="submit">
         Add to cart
       </Button>
     );
-
-    expect(screen.getByText(/Add to cart/i)).toBeDefined();
-
+  }),
+    test("should show title all the time", () => {
+      expect(screen.getByText(/Add to cart/i)).toBeDefined();
+    });
+  test("should the funcions been fired", () => {
     expect(handleClick).toHaveBeenCalledTimes(0);
     fireEvent.click(screen.getByText(/Add to cart/i));
     expect(handleClick).toHaveBeenCalledTimes(1);
